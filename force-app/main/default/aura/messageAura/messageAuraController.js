f12b95c53f080589bba5ec59fc0bad1f7ea930bd
Manish Pal <manish_pal@epam.com>
2022-04-21 19:40:36 +0530

    fixed the missing from value when passing the message from Aura comp

--- force-app/main/default/aura/messageAura/messageAuraController.js
+++ force-app/main/default/aura/messageAura/messageAuraController.js
@@ -28,7 +28,8 @@
             //constrcuting  message payload 
             const messagePayload = {
 
-                messages : msg
+                message : msg,
+                from:"AURA"
             };
 
             //we need to publish this msg on message channel
